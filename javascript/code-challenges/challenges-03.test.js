f623e40804638c7c18fa1a5c8d0a88ccb576246b
RIKI PLAZA <rikiplaza1@gmail.com>
2022-06-17 01:39:09 -0400

    lab 3 finished

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -9,9 +9,9 @@ Write a function called addTwo that takes in an array and adds two to every valu
 const addTwo = (arr) => {
   let newArr = [];
   for(let i = 0; i < arr.length; i++){
-    newArr.push(arr[i]+2);
-    return newArr;
+    newArr.push(arr[i] + 2);
   }
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -23,6 +23,7 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const typeNum = (arr) => {
+  return arr.filter(num => typeof num === 'number');
   // Solution code here...
 };
 
@@ -35,7 +36,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  // Solution code here...
+  return arr.filter(words => (words.includes('and')));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,7 +48,7 @@ For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const oddValues = (arr) => {
-  // Solution code here...
+  return arr.filter((num) => num % 2 === 1);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -59,7 +60,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter(num => forbiddenValues.includes(num)===false);
 };
 
 /* ------------------------------------------------------------------------------------------------
