35dbae59f4eb2b31ba9a1b2fc64eb3734d8e8cea
RIKI PLAZA <rikiplaza1@gmail.com>
2022-08-03 05:33:48 -0400

    finished with challenge added white board

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -4,11 +4,9 @@ class LinkedList:
     """
 
     def __init__(self):
-        # initialization here
+        # init here
         self.head = None
-
-    def append(self, value):
-        self.head =Node(value, self.head)
+        self.tail = None
 
     def __str__(self):
         text = ""
@@ -16,6 +14,7 @@ class LinkedList:
 
         while current is not None:
             text += "{ " + str(current.value) + " } -> "
+            print(text)
             current = current.next
         return text + "NULL"
 
@@ -33,11 +32,69 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def append(self, value):
+
+        new_node = Node(value)
+
+        # start  beginning or head
+        current = self.head
+
+        while current:
+            # next n/a
+            if not current.next:
+
+                current.next = new_node
+
+                break
+            else:
+                # moves to next
+                current = current.next
+
+    def insert_before(self, search_value, value):
+        node_two = Node(value)
+        if not self.head:
+            raise TargetError
+
+        if self.head.value == search_value:
+            self.insert(value)
+            return
+
+        # start  head || beginning
+        current = self.head
+
+        while current and current.next:
+            # if current node has a value it looks for value
+            if current.next.value == search_value:
+
+                node_two.next = current.next
+
+                current.next = node_two
+
+                break
+            # was told danger
+
+            else:
+                current = current.next
+        else:
+            raise TargetError
+
+    def insert_after(self, search_value, value):
+
+        current = self.head
+
+        while current:
+            if current.value == search_value:
+                self.insert_before(current.next.value, value)
+                return
+            else:
+                current = current.next
+        raise TargetError
 
 class Node:
     def __init__(self, value, next=None):
         self.value = value
+        self.next = next
 
 
-class TargetError:
+class TargetError(Exception):
     pass
