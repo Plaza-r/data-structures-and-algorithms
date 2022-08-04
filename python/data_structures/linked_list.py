a17b3ac8ca9c85be9a4beafb3350e86261d92e72
RIKI PLAZA <rikiplaza1@gmail.com>
2022-08-04 11:02:48 -0400

    all test pass and added white board

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -4,11 +4,8 @@ class LinkedList:
     """
 
     def __init__(self):
-        # initialization here
+        # start here
         self.head = None
-
-    def append(self, value):
-        self.head =Node(value, self.head)
 
     def __str__(self):
         text = ""
@@ -33,11 +30,97 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def insert_before(self, search_value, value):
+        node_two = Node(value)
+        if not self.head:
+            raise TargetError
+
+        if self.head.value == search_value:
+            self.insert(value)
+            return
+
+        current = self.head
+
+        while current and current.next:
+            if current.next.value == search_value:
+
+                node_two.next = current.next
+
+                current.next = node_two
+
+                return
+
+
+            else:
+                current = current.next
+
+        raise TargetError
+
+    def insert_before_first(self, target, new):
+        if self.head is None:
+            raise TargetError
+        current = self.head
+        if current.value == target:
+
+            while current is not None:
+                if current.value == target:
+                    new_node = Node(new)
+                    new_node.next = current.next
+                    current.next = new_node
+                    return f"Success"
+                current = current.next
+
+    def insert_after(self, target, new):
+        if self.head is None:
+            raise TargetError
+        current = self.head
+        while current is not None:
+            if current.value == target:
+                new_node = Node(new)
+                new_node.next = current.next
+                current.next = new_node
+                return f"Success"
+            current = current.next
+        raise TargetError
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(value)
+
+    def get_length(self):
+        length = 0
+        current = self.head
+        while current:
+            length += 1
+            current = current.next
+        return length
+
+    def kth_from_end(self, k):
+        if k < 0:
+            raise TargetError
+        length = self.get_length()
+        if k >= length:
+            raise TargetError
+        target_idx = (length - 1) - k
+        current_idx = 0
+        current = self.head
+        while current:
+            if current_idx == target_idx:
+                return current.value
+            current_idx += 1
+            current = current.next
+
 
 class Node:
-    def __init__(self, value, next=None):
+    def __init__(self, value):
         self.value = value
+        self.next = None
 
 
-class TargetError:
+class TargetError(Exception):
     pass
--- python/docs/linked_list_insertions/README.md
+++ python/docs/linked_list_insertions/README.md
@@ -1,1 +1,50 @@
+# Challenge 7 kth
+## Riki E Plaza
+# Challenge Summary
+
+### Write the following method for the Linked List class:
+-kth from end
+- argument: a number, k, as a parameter.
+- Return the node’s value that is k places from the tail of the linked list.
+- You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+### White Board
+![challenge 7](challenge7whtieboard.png)
+
+### EFficiency
+### Big 0
+
+- time: 0 (n) for each method because each list
+possibly need to be transverse
+
+- Space 0(1) for each method as the size doesn't change no matter how long list
+
+### Solution
+def kth_from_end(self, k):
+
+    if k < 0:
+
+        raise TargetError
+
+    length = self.get_length()
+
+    if k >= length:
+
+        raise TargetError
+
+    target_idx = (length - 1) - k
+
+    current_idx = 0
+
+    current = self.head
+
+    while current:
+
+        if current_idx == target_idx:
+
+            return current.value
+
+        current_idx += 1
+
+        current = current.next
 
