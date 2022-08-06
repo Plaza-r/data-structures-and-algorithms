e3dcc27bfaa9ebd727722aacda0ec2a84f96d4b3
RIKI PLAZA <rikiplaza1@gmail.com>
2022-08-06 02:32:18 -0400

    not done, haveing issues with test

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,24 @@
-from data_structures.binary_tree import BinaryTree
-
+from data_structures.binary_tree import BinaryTree, Node
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
+    def add(self, value):
+        def move(root, new_node):
+            if not root:
+                return
+            if new_node.value < root.value:
+                if root.left:
+                    move(root.left, new_node)
+                else:
+                    root.left = new_node
+            else:
+                if root.right:
+                    move(root.right, new_node)
+                else:
+                    root.right = new_node
 
-    def __init__(self):
-        # initialization here
-        pass
+        added_node = Node(value)
+        if not self.root:
+            self.root = added_node
+            return
+        move(self.root, added_node)
 
-    def some_method(self):
-        # method body here
-        pass
